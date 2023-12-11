e25a0d4c36340f59d2aef3e03469e6b2a2157e52
Alex Ge <alexalexgege@gmail.com>
2023-12-10 19:51:16 -0800

    add user input

--- src/main/java/shs/cs/DialogueSystem.java
+++ src/main/java/shs/cs/DialogueSystem.java
@@ -1,7 +1,7 @@
 package shs.cs;
 
 import java.util.Random;
-
+import java.util.Scanner;
 public class DialogueSystem {
     private static Random rand = new Random();
 
@@ -12,12 +12,15 @@ public class DialogueSystem {
         /*Initializes the neural network.
         Reads user input and processes it through the network.
         Generates a response based on the network's output*/
-
-        String test = "abc";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input your sentence, then hit enter:");
+        String test = scanner.next();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < test.length(); i++) {
             String res = generateResponse(encodeUserInput(test.substring(i, i + 1)));
-            System.out.println("input char: " + test.substring(i, i + 1) + ", predicted char: " + res);
+            sb.append(res);
         }
+        System.out.println("input String: " + test + ", predicted String: " + sb.toString());
     }
 
     private static double[] encodeUserInput(String input) {
@@ -31,12 +34,12 @@ public class DialogueSystem {
     private static String generateResponse(double[] intent) {
         /*Determines the response based on the highest value in the intent array
         (representing the network's output). Create 3-4 responses of your own to choose from as an output.*/
-        int layerCount = rand.nextInt(3, 10);
+        int layerCount = rand.nextInt(8) + 3;
         int[] layerSizes = new int[layerCount];
 
         layerSizes[0] = intent.length; // first layer is input layer, match input size
         for (int i = 1; i < layerCount - 1; i++) {
-            layerSizes[i] = rand.nextInt(1, 20);
+            layerSizes[i] = rand.nextInt(20) + 1;
         }
         layerSizes[layerCount - 1] = 26; // last layer is 26, each for one of the 26 characters
 
